3d1838c3de1aea741e4031b7944ac377c15e841a
Ubuntu <ubuntu@ip-172-31-35-177.us-west-2.compute.internal>
2013-07-09 14:05:40 +0000

    subm web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
-    var js = require('fs');
+    var fs = require('fs');
     fs.readfile(index.html);
 });
 
